118af93e747913f7c8dc9dc0a4ba3eea06402133
florindragos <florind@uhurusoftware.com>
2014-08-21 15:09:28 +0300

    Add some backend tests

--- backend/backend_test.go
+++ backend/backend_test.go
@@ -1,11 +1,307 @@
 package backend_test
 
-// import (
-// 	. "github.com/cloudfoundry-incubator/warden-windows/backend"
-// 	. "github.com/onsi/ginkgo"
-// 	. "github.com/onsi/gomega"
-// )
+import (
+	"bufio"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
 
-// var _ = Describe("Backend", func() {
+	"github.com/cloudfoundry-incubator/cf-lager"
+	WardenClient "github.com/cloudfoundry-incubator/garden/client"
+	WardenConnection "github.com/cloudfoundry-incubator/garden/client/connection"
+	Garden "github.com/cloudfoundry-incubator/garden/server"
+	Warden "github.com/cloudfoundry-incubator/garden/warden"
 
-// })
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/uhurusoftware/warden-windows/backend"
+)
+
+var _ = Describe("Backend", func() {
+	var (
+		s   *Garden.WardenServer
+		err error
+	)
+
+	BeforeSuite(func() {
+		defer GinkgoRecover()
+		backend := New("c:\\warden", nil)
+		logger := cf_lager.New("warden-winodws")
+		serverLogger := logger.Session("garden")
+		s = Garden.New("tcp", "0.0.0.0:9877", 0, backend, serverLogger)
+
+		err = s.Start()
+		Ω(err).ShouldNot(HaveOccurred())
+
+	})
+
+	AfterSuite(func() {
+		defer GinkgoRecover()
+		s.Stop()
+
+	})
+
+	Describe("connect to containers", func() {
+		var (
+			connection WardenConnection.Connection
+			client     Warden.Client
+			container  Warden.Container
+			tempDir    string
+		)
+
+		BeforeEach(func() {
+			defer GinkgoRecover()
+			connection = WardenConnection.New("tcp", "127.0.0.1:9877")
+			client = WardenClient.New(connection)
+			containerSpec := Warden.ContainerSpec{}
+			container, err = client.Create(containerSpec)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			tempDir = rand_str(10)
+			err = os.MkdirAll(tempDir, 0666)
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			defer GinkgoRecover()
+			client.Destroy(container.Handle())
+			os.RemoveAll(tempDir)
+		})
+
+		It("should run ping inside container", func() {
+			stdinRead, _ := io.Pipe()
+			stdoutRead, stdoutWrite := io.Pipe()
+			stderrRead, stderrWrite := io.Pipe()
+			processIo := Warden.ProcessIO{Stdin: stdinRead, Stdout: stdoutWrite, Stderr: stderrWrite}
+
+			processSpec := Warden.ProcessSpec{Path: "\\..\\..\\Windows\\System32\\ping.exe", Args: []string{"127.0.0.1"}}
+			process, err := container.Run(processSpec, processIo)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			go func() {
+				r := bufio.NewReader(stdoutRead)
+
+				for {
+					x, _, err := r.ReadLine()
+					if err != nil {
+						Ω(err).ShouldNot(HaveOccurred())
+					}
+					fmt.Println(string(x))
+				}
+			}()
+
+			go func() {
+				r := bufio.NewReader(stderrRead)
+
+				for {
+					x, _, err := r.ReadLine()
+					if err != nil {
+						Ω(err).ShouldNot(HaveOccurred())
+					}
+					fmt.Println(string(x))
+				}
+			}()
+
+			process.Wait()
+
+		})
+
+		It("should run cmd and echo something", func() {
+			stdinRead, stdinWrite := io.Pipe()
+			stdoutRead, stdoutWrite := io.Pipe()
+			stderrRead, stderrWrite := io.Pipe()
+			processIo := Warden.ProcessIO{Stdin: stdinRead, Stdout: stdoutWrite, Stderr: stderrWrite}
+			processSpec := Warden.ProcessSpec{Path: "\\..\\..\\Windows\\System32\\cmd.exe"}
+			_, err := container.Run(processSpec, processIo)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			w := bufio.NewWriter(stdinWrite)
+			fmt.Fprint(w, "echo ###test###\n")
+			w.Flush()
+
+			go func() {
+				defer GinkgoRecover()
+				defer container.Stop(true)
+				r := bufio.NewReader(stderrRead)
+
+				for {
+					x, _, err := r.ReadLine()
+					if err != nil {
+						Ω(err).ShouldNot(HaveOccurred())
+					}
+					fmt.Println(string(x))
+				}
+			}()
+
+			timeout := make(chan bool)
+			success := make(chan bool)
+
+			go func() {
+				time.Sleep(5 * time.Second)
+				timeout <- true
+			}()
+
+			r := bufio.NewReader(stdoutRead)
+
+			go func() {
+				for {
+
+					x, _, err := r.ReadLine()
+					if err != nil {
+						Ω(err).ShouldNot(HaveOccurred())
+					}
+					if x != nil {
+						result := string(x)
+						if result == "###test###" {
+							success <- true
+						}
+					}
+
+					fmt.Println(string(x))
+				}
+			}()
+
+			for {
+				select {
+				case <-timeout:
+					{
+						Fail("timeout")
+					}
+				case <-success:
+					{
+						return
+					}
+				default:
+				}
+			}
+		})
+
+		It("should stream in", func() {
+			destPath := "streamintest"
+			testString := "Hello cartridge"
+			reader, err := GetTarStreamFromString(testString, tempDir, destPath)
+			Ω(err).ShouldNot(HaveOccurred())
+			err = container.StreamIn(destPath, reader)
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+
+		It("should stream out", func() {
+			destPath := "streamouttest"
+			testString := "Hello cartridge"
+
+			reader, err := GetTarStreamFromString(testString, tempDir, destPath)
+			Ω(err).ShouldNot(HaveOccurred())
+			err = container.StreamIn(destPath, reader)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			reader, err = container.StreamOut(destPath)
+			result, err := GetStringFromTarStream(reader, tempDir, destPath)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(result).Should(Equal(testString))
+
+		})
+	})
+})
+
+func GetTarStreamFromString(text string, tempDir string, filename string) (io.Reader, error) {
+
+	fmt.Println(tempDir)
+	f, err := os.Create(filepath.Join(tempDir, filename))
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.WriteString(f, text)
+	if err != nil {
+		return nil, err
+	}
+	f.Close()
+
+	workingDir := tempDir
+	compressArg := filename
+
+	tarRead, tarWrite := io.Pipe()
+
+	tarPath := "C:\\Program Files (x86)\\Git\\bin\\tar.exe"
+	cmdPath := "C:\\Windows\\System32\\cmd.exe"
+
+	cmd := &exec.Cmd{
+		Path: cmdPath,
+		// Dir:  workingDir,
+		Args: []string{
+			"/c",
+			tarPath,
+			"cf",
+			"-",
+			"-C",
+			workingDir,
+			compressArg,
+		},
+		Stdout: tarWrite,
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		cmd.Wait()
+		tarWrite.Close()
+	}()
+
+	return tarRead, nil
+}
+
+func GetStringFromTarStream(source io.Reader, tempDir string, filename string) (string, error) {
+	absDestPath := filepath.Join(tempDir, "result")
+
+	err := os.MkdirAll(absDestPath, 0777)
+	if err != nil {
+		return "", err
+	}
+
+	tarPath := "C:\\Program Files (x86)\\Git\\bin\\tar.exe"
+	cmdPath := "C:\\Windows\\System32\\cmd.exe"
+
+	cmd := &exec.Cmd{
+		Path: cmdPath,
+		Dir:  absDestPath,
+		Args: []string{
+			"/c",
+			tarPath,
+			"xf",
+			"-",
+			"-C",
+			"./",
+		},
+		Stdin: source,
+	}
+
+	err = cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	buf, err := ioutil.ReadFile(filepath.Join(absDestPath, filename, filename))
+	if err != nil {
+		return "", err
+	}
+	value := string(buf)
+
+	return value, nil
+}
+
+func rand_str(str_size int) string {
+	alphanum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, str_size)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
+}
--- backend/container_test.go
+++ backend/container_test.go
@@ -1,352 +1,353 @@
 package backend_test
 
-import (
-	"errors"
-	"io"
-	"net/rpc"
-	"net/rpc/jsonrpc"
-	"os/exec"
-
-	"github.com/cloudfoundry-incubator/garden/warden"
-	"github.com/cloudfoundry/gunk/command_runner/fake_command_runner"
-	. "github.com/cloudfoundry/gunk/command_runner/fake_command_runner/matchers"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
-	. "github.com/uhurusoftware/warden-windows/backend"
-)
-
-type FakeContainerServer struct {
-	WhenStopping func(request *messages.StopRequest, response *messages.StopResponse) error
-	WhenRunning  func(request *messages.RunRequest, response *messages.RunResponse) error
-}
-
-func (server FakeContainerServer) Stop(request *messages.StopRequest, response *messages.StopResponse) error {
-	defer GinkgoRecover()
-
-	if server.WhenStopping != nil {
-		return server.WhenStopping(request, response)
-	}
-
-	return nil
-}
-
-func (server FakeContainerServer) Run(request *messages.RunRequest, response *messages.RunResponse) error {
-	defer GinkgoRecover()
-
-	if server.WhenRunning != nil {
-		return server.WhenRunning(request, response)
-	}
-
-	return nil
-}
-
-var _ = Describe("Container", func() {
-	var runner *fake_command_runner.FakeCommandRunner
-	var muxer *fake_muxer.FakeMuxer
-
-	var container *Container
-
-	BeforeEach(func() {
-		runner = fake_command_runner.New()
-		muxer = fake_muxer.New()
-		container = NewContainer("some-id", "some-handle", "some-container-path", runner, muxer)
-	})
-
-	Describe("Start", func() {
-		It("spawns a daemon with the correct handle", func() {
-			err := container.Start("DAEMON_PATH")
-			Ω(err).ShouldNot(HaveOccurred())
-
-			Ω(runner).Should(HaveStartedExecuting(
-				fake_command_runner.CommandSpec{
-					Path: "DAEMON_PATH",
-					Args: []string{"--handle", "some-handle", "--rootPath", "some-container-path"},
-				},
-			))
-		})
-
-		It("hooks into the daemon's stdin/stdout", func() {
-			runner.WhenRunning(
-				fake_command_runner.CommandSpec{
-					Path: "DAEMON_PATH",
-				}, func(cmd *exec.Cmd) error {
-					Ω(cmd.Stdin).ShouldNot(BeNil())
-					Ω(cmd.Stdout).ShouldNot(BeNil())
-					Ω(cmd.Stderr).ShouldNot(BeNil())
-					return nil
-				},
-			)
-
-			err := container.Start("DAEMON_PATH")
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-
-		Context("when spawning the daemon fails", func() {
-			disaster := errors.New("oh no!")
-
-			BeforeEach(func() {
-				runner.WhenRunning(
-					fake_command_runner.CommandSpec{
-						Path: "DAEMON_PATH",
-					}, func(cmd *exec.Cmd) error {
-						return disaster
-					},
-				)
-			})
-
-			It("returns the error", func() {
-				err := container.Start("DAEMON_PATH")
-				Ω(err).Should(Equal(disaster))
-			})
-		})
-	})
-
-	Describe("after starting", func() {
-		var containerServer *FakeContainerServer
-		var server *rpc.Server
-
-		var processPayloadStream io.Writer
-
-		BeforeEach(func() {
-			containerServer = new(FakeContainerServer)
-
-			server = rpc.NewServer()
-			server.RegisterName("Container", containerServer)
-
-			runner.WhenRunning(
-				fake_command_runner.CommandSpec{
-					Path: "DAEMON_PATH",
-				}, func(cmd *exec.Cmd) error {
-					go server.ServeCodec(jsonrpc.NewServerCodec(iorpc.New(cmd.Stdout.(io.WriteCloser), cmd.Stdin.(io.ReadCloser))))
-
-					processPayloadStream = cmd.Stderr
-
-					return nil
-				},
-			)
-
-			err := container.Start("DAEMON_PATH")
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-
-		Describe("Stop", func() {
-			It("sends a Stop message to the container", func() {
-				calledStop := false
-
-				containerServer.WhenStopping = func(request *messages.StopRequest, response *messages.StopResponse) error {
-					calledStop = true
-					return nil
-				}
-
-				err := container.Stop(false)
-				Ω(err).ShouldNot(HaveOccurred())
-
-				Ω(calledStop).Should(BeTrue())
-			})
-
-			Context("when told to kill", func() {
-				It("sends the message with Kill true", func() {
-					containerServer.WhenStopping = func(request *messages.StopRequest, response *messages.StopResponse) error {
-						Ω(request.Kill).Should(BeTrue())
-
-						return nil
-					}
-
-					err := container.Stop(true)
-					Ω(err).ShouldNot(HaveOccurred())
-				})
-			})
-
-			Context("when the RPC fails", func() {
-				disaster := errors.New("oh no!")
-
-				BeforeEach(func() {
-					containerServer.WhenStopping = func(request *messages.StopRequest, response *messages.StopResponse) error {
-						return disaster
-					}
-				})
-
-				It("returns an error", func() {
-					err := container.Stop(true)
-					Ω(err.Error()).Should(Equal(disaster.Error()))
-				})
-			})
-		})
-
-		Describe("Run", func() {
-			It("sends a Run message to the container", func() {
-				calledRun := false
-
-				containerServer.WhenRunning = func(request *messages.RunRequest, response *messages.RunResponse) error {
-					calledRun = true
-					response.ProcessID = 42
-					return nil
-				}
-
-				pid, _, err := container.Run(warden.ProcessSpec{
-					Script:     "rm -rf /",
-					Privileged: true,
-				})
-				Ω(err).ShouldNot(HaveOccurred())
-
-				Ω(calledRun).Should(BeTrue())
-				Ω(pid).Should(Equal(uint32(42)))
-			})
-
-			It("sends the script and privileged along", func() {
-				containerServer.WhenRunning = func(request *messages.RunRequest, response *messages.RunResponse) error {
-					Ω(request.Script).Should(Equal("rm -rf /"))
-					Ω(request.Privileged).Should(BeTrue())
-					return nil
-				}
-
-				_, _, err := container.Run(warden.ProcessSpec{
-					Script:     "rm -rf /",
-					Privileged: true,
-				})
-				Ω(err).ShouldNot(HaveOccurred())
-			})
-
-			It("subscribes to the process id's payloads and forwards the stream", func() {
-				processID, stream, err := container.Run(warden.ProcessSpec{
-					Script:     "rm -rf /",
-					Privileged: true,
-				})
-				Ω(err).ShouldNot(HaveOccurred())
-
-				subscribers := muxer.Subscribers(processID)
-				Ω(subscribers).Should(HaveLen(1))
-
-				writeEnd := subscribers[0]
-
-				go func() {
-					writeEnd <- warden.ProcessStream{
-						Source: warden.ProcessStreamSourceStdout,
-						Data:   []byte("stdout data for 42"),
-					}
-
-					writeEnd <- warden.ProcessStream{
-						Source: warden.ProcessStreamSourceStderr,
-						Data:   []byte("stderr data for 42"),
-					}
-
-					exitStatus := uint32(142)
-
-					writeEnd <- warden.ProcessStream{
-						ExitStatus: &exitStatus,
-					}
-				}()
-
-				var payload warden.ProcessStream
-				Eventually(stream).Should(Receive(&payload))
-				Ω(payload.Source).Should(Equal(warden.ProcessStreamSourceStdout))
-				Ω(string(payload.Data)).Should(Equal("stdout data for 42"))
-
-				Eventually(stream).Should(Receive(&payload))
-				Ω(payload.Source).Should(Equal(warden.ProcessStreamSourceStderr))
-				Ω(string(payload.Data)).Should(Equal("stderr data for 42"))
-
-				Eventually(stream).Should(Receive(&payload))
-				Ω(payload.ExitStatus).ShouldNot(BeNil())
-				Ω(*payload.ExitStatus).Should(Equal(uint32(142)))
-			})
-
-			It("subscribes with a buffer of 1000", func(done Done) {
-				defer close(done)
-
-				processID, _, err := container.Run(warden.ProcessSpec{
-					Script:     "rm -rf /",
-					Privileged: true,
-				})
-				Ω(err).ShouldNot(HaveOccurred())
-
-				subscribers := muxer.Subscribers(processID)
-				Ω(subscribers).Should(HaveLen(1))
-
-				writeEnd := subscribers[0]
-
-				for i := 0; i < 1000; i++ {
-					writeEnd <- warden.ProcessStream{
-						Source: warden.ProcessStreamSourceStdout,
-						Data:   []byte("stdout data for 42"),
-					}
-				}
-
-				select {
-				case writeEnd <- warden.ProcessStream{}:
-					Fail("channel should have been clogged")
-				default:
-				}
-			}, 2.0)
-		})
-
-		Describe("Attach", func() {
-			It("subscribes to the process id's payloads and forwards the stream", func() {
-				stream, err := container.Attach(12)
-				Ω(err).ShouldNot(HaveOccurred())
-
-				subscribers := muxer.Subscribers(12)
-				Ω(subscribers).Should(HaveLen(1))
-
-				writeEnd := subscribers[0]
-
-				go func() {
-					writeEnd <- warden.ProcessStream{
-						Source: warden.ProcessStreamSourceStdout,
-						Data:   []byte("stdout data for 42"),
-					}
-
-					writeEnd <- warden.ProcessStream{
-						Source: warden.ProcessStreamSourceStderr,
-						Data:   []byte("stderr data for 42"),
-					}
-
-					exitStatus := uint32(142)
-
-					writeEnd <- warden.ProcessStream{
-						ExitStatus: &exitStatus,
-					}
-				}()
-
-				var payload warden.ProcessStream
-				Eventually(stream).Should(Receive(&payload))
-				Ω(payload.Source).Should(Equal(warden.ProcessStreamSourceStdout))
-				Ω(string(payload.Data)).Should(Equal("stdout data for 42"))
-
-				Eventually(stream).Should(Receive(&payload))
-				Ω(payload.Source).Should(Equal(warden.ProcessStreamSourceStderr))
-				Ω(string(payload.Data)).Should(Equal("stderr data for 42"))
-
-				Eventually(stream).Should(Receive(&payload))
-				Ω(payload.ExitStatus).ShouldNot(BeNil())
-				Ω(*payload.ExitStatus).Should(Equal(uint32(142)))
-			})
-
-			It("subscribes with a buffer of 1000", func(done Done) {
-				defer close(done)
-
-				_, err := container.Attach(12)
-				Ω(err).ShouldNot(HaveOccurred())
-
-				subscribers := muxer.Subscribers(12)
-				Ω(subscribers).Should(HaveLen(1))
-
-				writeEnd := subscribers[0]
-
-				for i := 0; i < 1000; i++ {
-					writeEnd <- warden.ProcessStream{
-						Source: warden.ProcessStreamSourceStdout,
-						Data:   []byte("stdout data for 42"),
-					}
-				}
-
-				select {
-				case writeEnd <- warden.ProcessStream{}:
-					Fail("channel should have been clogged")
-				default:
-				}
-			}, 2.0)
-		})
-	})
-})
+//import (
+//	"errors"
+//	"io"
+//	"net/rpc"
+//	"net/rpc/jsonrpc"
+//	"os/exec"
+
+//	"github.com/cloudfoundry-incubator/garden/warden"
+//	"github.com/cloudfoundry/gunk/command_runner/fake_command_runner"
+//	. "github.com/cloudfoundry/gunk/command_runner/fake_command_runner/matchers"
+//	. "github.com/onsi/ginkgo"
+//	. "github.com/onsi/gomega"
+
+//	. "github.com/uhurusoftware/warden-windows/backend"
+//)
+
+//type FakeContainerServer struct {
+//	WhenStopping func(request *messages.StopRequest, response *messages.StopResponse) error
+
+//	WhenRunning func(request *messages.RunRequest, response *messages.RunResponse) error
+//}
+
+//func (server FakeContainerServer) Stop(request *messages.StopRequest, response *messages.StopResponse) error {
+//	defer GinkgoRecover()
+
+//	if server.WhenStopping != nil {
+//		return server.WhenStopping(request, response)
+//	}
+
+//	return nil
+//}
+
+//func (server FakeContainerServer) Run(request *messages.RunRequest, response *messages.RunResponse) error {
+//	defer GinkgoRecover()
+
+//	if server.WhenRunning != nil {
+//		return server.WhenRunning(request, response)
+//	}
+
+//	return nil
+//}
+
+//var _ = Describe("Container", func() {
+//	var runner *fake_command_runner.FakeCommandRunner
+//	var muxer *fake_muxer.FakeMuxer
+
+//	var container *Container
+
+//	BeforeEach(func() {
+//		runner = fake_command_runner.New()
+//		muxer = fake_muxer.New()
+//		container = NewContainer("some-id", "some-handle", "some-container-path", runner, muxer)
+//	})
+
+//	Describe("Start", func() {
+//		It("spawns a daemon with the correct handle", func() {
+//			err := container.Start("DAEMON_PATH")
+//			Ω(err).ShouldNot(HaveOccurred())
+
+//			Ω(runner).Should(HaveStartedExecuting(
+//				fake_command_runner.CommandSpec{
+//					Path: "DAEMON_PATH",
+//					Args: []string{"--handle", "some-handle", "--rootPath", "some-container-path"},
+//				},
+//			))
+//		})
+
+//		It("hooks into the daemon's stdin/stdout", func() {
+//			runner.WhenRunning(
+//				fake_command_runner.CommandSpec{
+//					Path: "DAEMON_PATH",
+//				}, func(cmd *exec.Cmd) error {
+//					Ω(cmd.Stdin).ShouldNot(BeNil())
+//					Ω(cmd.Stdout).ShouldNot(BeNil())
+//					Ω(cmd.Stderr).ShouldNot(BeNil())
+//					return nil
+//				},
+//			)
+
+//			err := container.Start("DAEMON_PATH")
+//			Ω(err).ShouldNot(HaveOccurred())
+//		})
+
+//		Context("when spawning the daemon fails", func() {
+//			disaster := errors.New("oh no!")
+
+//			BeforeEach(func() {
+//				runner.WhenRunning(
+//					fake_command_runner.CommandSpec{
+//						Path: "DAEMON_PATH",
+//					}, func(cmd *exec.Cmd) error {
+//						return disaster
+//					},
+//				)
+//			})
+
+//			It("returns the error", func() {
+//				err := container.Start("DAEMON_PATH")
+//				Ω(err).Should(Equal(disaster))
+//			})
+//		})
+//	})
+
+//	Describe("after starting", func() {
+//		var containerServer *FakeContainerServer
+//		var server *rpc.Server
+
+//		var processPayloadStream io.Writer
+
+//		BeforeEach(func() {
+//			containerServer = new(FakeContainerServer)
+
+//			server = rpc.NewServer()
+//			server.RegisterName("Container", containerServer)
+
+//			runner.WhenRunning(
+//				fake_command_runner.CommandSpec{
+//					Path: "DAEMON_PATH",
+//				}, func(cmd *exec.Cmd) error {
+//					go server.ServeCodec(jsonrpc.NewServerCodec(iorpc.New(cmd.Stdout.(io.WriteCloser), cmd.Stdin.(io.ReadCloser))))
+
+//					processPayloadStream = cmd.Stderr
+
+//					return nil
+//				},
+//			)
+
+//			err := container.Start("DAEMON_PATH")
+//			Ω(err).ShouldNot(HaveOccurred())
+//		})
+
+//		Describe("Stop", func() {
+//			It("sends a Stop message to the container", func() {
+//				calledStop := false
+
+//				containerServer.WhenStopping = func(request *messages.StopRequest, response *messages.StopResponse) error {
+//					calledStop = true
+//					return nil
+//				}
+
+//				err := container.Stop(false)
+//				Ω(err).ShouldNot(HaveOccurred())
+
+//				Ω(calledStop).Should(BeTrue())
+//			})
+
+//			Context("when told to kill", func() {
+//				It("sends the message with Kill true", func() {
+//					containerServer.WhenStopping = func(request *messages.StopRequest, response *messages.StopResponse) error {
+//						Ω(request.Kill).Should(BeTrue())
+
+//						return nil
+//					}
+
+//					err := container.Stop(true)
+//					Ω(err).ShouldNot(HaveOccurred())
+//				})
+//			})
+
+//			Context("when the RPC fails", func() {
+//				disaster := errors.New("oh no!")
+
+//				BeforeEach(func() {
+//					containerServer.WhenStopping = func(request *messages.StopRequest, response *messages.StopResponse) error {
+//						return disaster
+//					}
+//				})
+
+//				It("returns an error", func() {
+//					err := container.Stop(true)
+//					Ω(err.Error()).Should(Equal(disaster.Error()))
+//				})
+//			})
+//		})
+
+//		Describe("Run", func() {
+//			It("sends a Run message to the container", func() {
+//				calledRun := false
+
+//				containerServer.WhenRunning = func(request *messages.RunRequest, response *messages.RunResponse) error {
+//					calledRun = true
+//					response.ProcessID = 42
+//					return nil
+//				}
+
+//				pid, _, err := container.Run(warden.ProcessSpec{
+//					Script:     "rm -rf /",
+//					Privileged: true,
+//				})
+//				Ω(err).ShouldNot(HaveOccurred())
+
+//				Ω(calledRun).Should(BeTrue())
+//				Ω(pid).Should(Equal(uint32(42)))
+//			})
+
+//			It("sends the script and privileged along", func() {
+//				containerServer.WhenRunning = func(request *messages.RunRequest, response *messages.RunResponse) error {
+//					Ω(request.Script).Should(Equal("rm -rf /"))
+//					Ω(request.Privileged).Should(BeTrue())
+//					return nil
+//				}
+
+//				_, _, err := container.Run(warden.ProcessSpec{
+//					Script:     "rm -rf /",
+//					Privileged: true,
+//				})
+//				Ω(err).ShouldNot(HaveOccurred())
+//			})
+
+//			It("subscribes to the process id's payloads and forwards the stream", func() {
+//				processID, stream, err := container.Run(warden.ProcessSpec{
+//					Script:     "rm -rf /",
+//					Privileged: true,
+//				})
+//				Ω(err).ShouldNot(HaveOccurred())
+
+//				subscribers := muxer.Subscribers(processID)
+//				Ω(subscribers).Should(HaveLen(1))
+
+//				writeEnd := subscribers[0]
+
+//				go func() {
+//					writeEnd <- warden.ProcessStream{
+//						Source: warden.ProcessStreamSourceStdout,
+//						Data:   []byte("stdout data for 42"),
+//					}
+
+//					writeEnd <- warden.ProcessStream{
+//						Source: warden.ProcessStreamSourceStderr,
+//						Data:   []byte("stderr data for 42"),
+//					}
+
+//					exitStatus := uint32(142)
+
+//					writeEnd <- warden.ProcessStream{
+//						ExitStatus: &exitStatus,
+//					}
+//				}()
+
+//				var payload warden.ProcessStream
+//				Eventually(stream).Should(Receive(&payload))
+//				Ω(payload.Source).Should(Equal(warden.ProcessStreamSourceStdout))
+//				Ω(string(payload.Data)).Should(Equal("stdout data for 42"))
+
+//				Eventually(stream).Should(Receive(&payload))
+//				Ω(payload.Source).Should(Equal(warden.ProcessStreamSourceStderr))
+//				Ω(string(payload.Data)).Should(Equal("stderr data for 42"))
+
+//				Eventually(stream).Should(Receive(&payload))
+//				Ω(payload.ExitStatus).ShouldNot(BeNil())
+//				Ω(*payload.ExitStatus).Should(Equal(uint32(142)))
+//			})
+
+//			It("subscribes with a buffer of 1000", func(done Done) {
+//				defer close(done)
+
+//				processID, _, err := container.Run(warden.ProcessSpec{
+//					Script:     "rm -rf /",
+//					Privileged: true,
+//				})
+//				Ω(err).ShouldNot(HaveOccurred())
+
+//				subscribers := muxer.Subscribers(processID)
+//				Ω(subscribers).Should(HaveLen(1))
+
+//				writeEnd := subscribers[0]
+
+//				for i := 0; i < 1000; i++ {
+//					writeEnd <- warden.ProcessStream{
+//						Source: warden.ProcessStreamSourceStdout,
+//						Data:   []byte("stdout data for 42"),
+//					}
+//				}
+
+//				select {
+//				case writeEnd <- warden.ProcessStream{}:
+//					Fail("channel should have been clogged")
+//				default:
+//				}
+//			}, 2.0)
+//		})
+
+//		Describe("Attach", func() {
+//			It("subscribes to the process id's payloads and forwards the stream", func() {
+//				stream, err := container.Attach(12)
+//				Ω(err).ShouldNot(HaveOccurred())
+
+//				subscribers := muxer.Subscribers(12)
+//				Ω(subscribers).Should(HaveLen(1))
+
+//				writeEnd := subscribers[0]
+
+//				go func() {
+//					writeEnd <- warden.ProcessStream{
+//						Source: warden.ProcessStreamSourceStdout,
+//						Data:   []byte("stdout data for 42"),
+//					}
+
+//					writeEnd <- warden.ProcessStream{
+//						Source: warden.ProcessStreamSourceStderr,
+//						Data:   []byte("stderr data for 42"),
+//					}
+
+//					exitStatus := uint32(142)
+
+//					writeEnd <- warden.ProcessStream{
+//						ExitStatus: &exitStatus,
+//					}
+//				}()
+
+//				var payload warden.ProcessStream
+//				Eventually(stream).Should(Receive(&payload))
+//				Ω(payload.Source).Should(Equal(warden.ProcessStreamSourceStdout))
+//				Ω(string(payload.Data)).Should(Equal("stdout data for 42"))
+
+//				Eventually(stream).Should(Receive(&payload))
+//				Ω(payload.Source).Should(Equal(warden.ProcessStreamSourceStderr))
+//				Ω(string(payload.Data)).Should(Equal("stderr data for 42"))
+
+//				Eventually(stream).Should(Receive(&payload))
+//				Ω(payload.ExitStatus).ShouldNot(BeNil())
+//				Ω(*payload.ExitStatus).Should(Equal(uint32(142)))
+//			})
+
+//			It("subscribes with a buffer of 1000", func(done Done) {
+//				defer close(done)
+
+//				_, err := container.Attach(12)
+//				Ω(err).ShouldNot(HaveOccurred())
+
+//				subscribers := muxer.Subscribers(12)
+//				Ω(subscribers).Should(HaveLen(1))
+
+//				writeEnd := subscribers[0]
+
+//				for i := 0; i < 1000; i++ {
+//					writeEnd <- warden.ProcessStream{
+//						Source: warden.ProcessStreamSourceStdout,
+//						Data:   []byte("stdout data for 42"),
+//					}
+//				}
+
+//				select {
+//				case writeEnd <- warden.ProcessStream{}:
+//					Fail("channel should have been clogged")
+//				default:
+//				}
+//			}, 2.0)
+//		})
+//	})
+//})
